2f530618f1da9e8c82fb5b1d653d766230025123
ismail-dev-code <ismail.online0385678@gmail.com>
2024-11-30 10:26:18 +0600

    looping

---
+++ for-of.js
@@ -0,0 +1,11 @@
+// const numbers = [2, 32, 20, 22, 35]
+// console.log(numbers); 
+// for(const num of numbers){
+//     console.log(num);
+// }
+
+const fruits = ['jambura', 'orange', 'apple', 'mango'];
+for(const fruit of fruits){
+    console.log(fruit);
+    console.log('I want to eat');
+}
---
+++ while.js
@@ -0,0 +1,5 @@
+const number = 9;
+if(number <1){
+    console.log('choto ekta number haisot');
+}
+console.log('something very important');

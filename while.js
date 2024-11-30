6c2f8c6bb39b7e92de827fcb872eb430d6255b92
ismail-dev-code <ismail.online0385678@gmail.com>
2024-11-30 18:33:54 +0600

    range sum added

--- while.js
+++ while.js
@@ -3,3 +3,9 @@ if(number <1){
     console.log('choto ekta number haisot');
 }
 console.log('something very important');
+
+// let num = 0;
+// while(num <5){
+//     console.log('loooping', num);
+//     num++
+// }
